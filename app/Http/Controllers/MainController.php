76a299e923ba25828ae27c2f96a053d81000b10a
Vitor Augusto <vaugusto.pereira17@gmail.com>
2024-11-19 16:16:46 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
 //Home
-    public function home() {
-
+    public function home(): View {
+        return view('Home');
     }
 //Gerar os exercícios
     public function generateExercises(Request $request) {
