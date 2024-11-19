df77e6a0b9339bec1d35d229940be3c79b4c9215
Vitor Augusto <vaugusto.pereira17@gmail.com>
2024-11-19 16:06:48 -0300

    002

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+//Home
+    public function home() {
+
+    }
+//Gerar os exercícios
+    public function generateExercises(Request $request) {
+
+    }
+//Imprimir
+    public function printExercises() {
+
+    }
+
+//Exporta-los
+    public function exportExercises() {
+
+    }
+}
